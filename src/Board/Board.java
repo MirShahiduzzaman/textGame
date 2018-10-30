a0f22f419d2d2a84b8a66da6a688b6ca1beedec1
Mir Shahiduzzaman <mshahiduzzaman8068@bths.edu>
2018-10-30 10:15:46 -0400

    Made a Board class.
    Signed-off-by: Mir Shahiduzzaman <mshahiduzzaman8068@bths.edu>

---
+++ src/Board/Board.java
@@ -0,0 +1,12 @@
+package Board;
+
+public class Board
+{
+    String[][] chessBoard;
+
+    public Board(String[][] chessBoard)
+    {
+        this.chessBoard = chessBoard;
+    }
+    
+}
--- src/People/Monster.java
+++ src/People/Monster.java
@@ -1,5 +1,35 @@
 package People;
 
+/**
+ * Monster chases person and is shown on the map name: Mad HorseMan
+ */
 public class Monster
 {
+    String firstName;
+    String familyName;
+    int xLoc, yLoc;
+
+    public int getxLoc() {
+        return xLoc;
+    }
+
+    public void setxLoc(int xLoc) {
+        this.xLoc = xLoc;
+    }
+
+    public int getyLoc() {
+        return yLoc;
+    }
+
+    public void setyLoc(int yLoc) {
+        this.yLoc = yLoc;
+    }
+
+    public Monster (String firstName, String familyName, int xLoc, int yLoc)
+    {
+        this.firstName = firstName;
+        this.familyName = familyName;
+        this.xLoc = xLoc;
+        this.yLoc = yLoc;
+    }
 }
