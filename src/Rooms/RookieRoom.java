1f4a4570f29d92b8fa7487e5e2b84e99d3b7f237
Mir Shahiduzzaman <mshahiduzzaman8068@bths.edu>
2018-11-04 22:16:59 -0800

    Made options for difficulty but commented it so it doesn't waste time. Also made some Classes.
    Signed-off-by: Mir Shahiduzzaman <mshahiduzzaman8068@bths.edu>

---
+++ src/Rooms/RookieRoom.java
@@ -0,0 +1,4 @@
+package Rooms;
+//wastes ur time
+public class RookieRoom {
+}
---
+++ src/Rooms/StategyRoom.java
@@ -0,0 +1,5 @@
+package Rooms;
+//you feel smarter but soon realize this is the horseman's strategy room AKA u find out he can only move in a small L
+// shape
+public class StategyRoom {
+}
