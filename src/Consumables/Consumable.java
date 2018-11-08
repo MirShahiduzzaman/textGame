4cf26dfcf63909eec577738b75192fe3005eeff9
Mir Shahiduzzaman <mshahiduzzaman8068@bths.edu>
2018-11-08 05:32:31 -0800

    Fixed Horseman Movements. Need to work on other Classes.
    Signed-off-by: Mir Shahiduzzaman <mshahiduzzaman8068@bths.edu>

--- src/Consumables/Consumable.java
+++ src/Consumables/Consumable.java
@@ -1,5 +1,8 @@
 package Consumables;
-
+//fill with gainHealth and gainPotent
+//gainHealth increases health or decreases health depends on int value
+//gainPotent increases chance to move twice!
+//Once you move twice 0% potent again
 public interface Consumable
 {
 
--- src/Rooms/RookieRoom.java
+++ src/Rooms/RookieRoom.java
@@ -1,10 +1,33 @@
 /**Mir Shahiduzzaman November 5*/
 package Rooms;
+
+import People.Monster;
+import People.Person;
+
 //wastes ur time
 public class RookieRoom extends Room
 {
     public RookieRoom(int x, int y)
     {
         super(x,y);
+    }
+
+    @Override
+    public void enterRoom(Person x) {
+        if(!(occupant == null))
+        {
+            System.out.println("THE MAD HORSEMAN CAUGHT YOU! TRY AGAIN.");
+            System.exit(0);
+        }
+        occupant = x;
+        strOccupant = x + "";
+        x.setxLoc(this.xLoc);
+        x.setyLoc(this.yLoc);
+        if(!(x instanceof Monster))
+        {
+            System.out.println("You are bombarded with memories and faint. The horse to made a second move.");
+        }
+
+        //make horseman move randomly or else it will be too hard!
     }
 }
